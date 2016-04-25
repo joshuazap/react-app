ce645691cfe06a02a58cfbc478895408bfae675a
Joshua Zapata <joshuazap@gmail.com>
2016-04-25 15:16:56 -0700

    Add initial tests and components for testing

---
+++ scripts/accordionComponent.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var React = require('react');
+
+var Accordion = React.createClass({
+  render: function() {
+    var panes = [];
+    var data = this.props.data || [];
+
+    data.forEach(function(item, index) {
+      panes.push(
+        <div key={index}>
+          <div className="accordion-header">
+            {item.name}
+          </div>
+          <div className="accordion-content">
+            {item.content}
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div className="accordion">
+        {panes}
+      </div>
+    );
+  }
+});
+
+module.exports = Accordion;
